e328584d270036bf20aab2ea327322d138bc484b
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-06-20 15:40:57 +0900

    mysqlコンテナの生成とフロントエンドソースコードのコピーまで

---
+++ backend/api/db.py
@@ -0,0 +1,17 @@
+import MySQLdb
+import time
+
+time.sleep(5)
+
+# 
+connection = MySQLdb(
+    host="db",
+    user="root",
+    passwd="M4e80Xr1jQcdBnPt",
+    db="python_db"
+)
+
+cursor = connection.cursor()
+
+
+
