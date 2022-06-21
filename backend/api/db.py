93d3c1909b9019d1fe22eb73393347ffc17d43d1
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-06-21 18:10:40 +0900

    SQLAlchemyのテスト一回目

--- backend/api/db.py
+++ backend/api/db.py
@@ -1,17 +1,15 @@
-import MySQLdb
-import time
-
-time.sleep(5)
-
-# 
-connection = MySQLdb(
-    host="db",
-    user="root",
-    passwd="M4e80Xr1jQcdBnPt",
-    db="python_db"
-)
-
-cursor = connection.cursor()
+from sqlalchemy import  create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Integer, String
+from sqlalchemy.orm import sessionmaker
 
 
+engine = create_engine("mysql://root:M4e80Xr1jQcdBnPt@db:3306/python_db")
 
+Base = declarative_base()
+
+SessionClass = sessionmaker(autocommit=false, autoflush=true, engine)  # セッションを作るクラスを作成
+session = SessionClass()
+
+print(session.query())
