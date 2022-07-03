940abd1d593ed90ebc78941dc57c2c3c4ad93e92
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-07-03 17:42:55 +0900

    postモデルの削除

--- backend/api/test.py
+++ backend/api/test.py
@@ -1,6 +1,7 @@
 from models import user
 from models import post
 
+# user, postテーブルの初期化
 if __name__=="__main__":
     user.Base.metadata.create_all(bind=user.engine)
     post.Base.metadata.create_all(bind=post.engine)
