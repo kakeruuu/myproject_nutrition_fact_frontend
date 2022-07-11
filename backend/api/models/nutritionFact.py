563db40e52750140174d2497d18562a6eb666022
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-07-11 18:02:56 +0900

    食品成分表DBのデータ型の修正。Trを0に変更。

--- backend/api/models/nutritionFact.py
+++ backend/api/models/nutritionFact.py
@@ -10,15 +10,20 @@ class NutritionFact(Base):
     }
 
     id = Column("id", Integer, primary_key=True, autoincrement=True)
-    food_name = Column("name", String(200))
-    age = Column("age", Integer)
+    food_name = Column("food_name", String(200))
+    disposal_rate = Column("age", Integer)
     email = Column("email", String(200))
     
     # TODO:食品成分表の数値であってほしいデータのデータ型がテキストになっているのを修正
     # to_sqlの段階でテキスト化？そもそもSQLAlchemyは使わない方がいい？
     # MySQLには問題はなくて、そもそもCSVのデータにテキストと数値の混ざった列があるのが問題
-    
-    # 以下のobjectには混在してる可能性あり
+    # 混じっていてもいい？混在しているとモデルの作成時に面倒になる？
+    # 原因：Tr(微量,トレース)というデータが含まれているため
+    # 今回はTrのものは０として表記して注釈を入れることにする。
+    # ↑Trを全て0にしてもデータ型オブジェクトのままだった。
+    # ↑-が含まれていたから？
+
+    # 以下のobjectには混在してる可能性あり(df)
     # id                                                 int64
     # food_name                                         object
     # disposal_rate                                      int64
@@ -47,7 +52,39 @@ class NutritionFact(Base):
     # zinc                                              object
     # copper                                            object
     # manganese                                         object
+    
+    # Tr変換後↓
+    # id                                                  int64
+    # food_name                                          object
+    # disposal_rate                                       int64
+    # energy(KJ)                                          int64
+    # energy(Kcal)                                        int64
+    # moisture                                          float64
+    # protein_by_amino_acid_composition                  object
+    # protein                                            object
+    # fatty_acid_tria_silglycerol_equivalent             object
+    # cholesterol                                        object
+    # lipid                                              object
+    # available_carbohydrates(single_sugar)              object
+    # available_carbohydrates_(mass_meters)              object
+    # available_carbohydrates_by_the_dispatch_method     object
+    # total_amount_of_dietary_fiber                      object
+    # sugar_alcohol                                      object
+    # carbohydrate                                      float64
+    # organic_acid                                       object
+    # ash                                               float64
+    # sodium                                             object
+    # potassium                                          object
+    # calcium                                            object
+    # magnesium                                          object
+    # rin                                                object
+    # iron                                               object
+    # zinc                                               object
+    # copper                                             object
+    # manganese                                          object
 
+    # そもそもモデルって何のために作るんだっけ？
+    # modelはDBとやり取りすための場所
     
 if __name__=="__main__":
     Base.metadata.create_all(bind=engine)
