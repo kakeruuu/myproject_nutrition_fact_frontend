67ee6f0d85746da6640a6494b0aef47521029ebb
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-07-26 18:37:06 +0900

    複数検索値での成分の検索方法を探す

--- backend/api/crud/nutritionFact.py
+++ backend/api/crud/nutritionFact.py
@@ -4,10 +4,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.models.nutritionFact import NutritionFact
 
-
+# 次回コンマで分けた検索値から値を取得するSqlを考える
 async def read_nutrition(q: str, db: AsyncSession):
+    q_list = q.split(",")
+    print(q_list)
+    # result: Result = await db.execute(
+    #     select(NutritionFact).filter(NutritionFact.food_name.contains(q))
+    #     )
     result: Result = await db.execute(
-        select(NutritionFact).filter(NutritionFact.food_name.contains(q))
+        select(NutritionFact).filter(NutritionFact.food_name.in_(("砂糖","にんじん")))
         )
-    
     return result.all()
