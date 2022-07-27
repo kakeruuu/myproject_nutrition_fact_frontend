3252399936d8e5a055c69fe14921fa1fbceb4689
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-07-27 18:28:58 +0900

    read_nutritionを複数検索値対応にする

--- backend/api/crud/nutritionFact.py
+++ backend/api/crud/nutritionFact.py
@@ -1,17 +1,20 @@
-from sqlalchemy import select
+from sqlalchemy import select, or_, and_
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.models.nutritionFact import NutritionFact
 
-# 次回コンマで分けた検索値から値を取得するSqlを考える
+# 検索食材名を含むデータを返す
+# 該当する食材を一旦表示して、ユーザーに正しい内容を再度選択してもらう
+# ユーザー側に見せるのはIDとfood_nameだけでいい？
+# 次回上記の機能について考える
 async def read_nutrition(q: str, db: AsyncSession):
     q_list = q.split(",")
-    print(q_list)
-    # result: Result = await db.execute(
-    #     select(NutritionFact).filter(NutritionFact.food_name.contains(q))
-    #     )
+    
+    filters = [NutritionFact.food_name.contains(l) for l in q_list]
+    
     result: Result = await db.execute(
-        select(NutritionFact).filter(NutritionFact.food_name.in_(("砂糖","にんじん")))
+        select(NutritionFact).filter(or_(*filters))
         )
+
     return result.all()
