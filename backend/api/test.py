b93ad02e50e75dfd88273aae77adc626e1a2a932
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-08-07 10:42:41 +0900

    食品成分表フロントエンドのプロジェクトを再構築

--- backend/api/test.py
+++
@@ -1,8 +0,0 @@
-from models import user
-from crud import user
-# from models import post
-
-# user, postテーブルの初期化
-if __name__=="__main__":
-    # user.Base.metadata.create_all(bind=user.engine)
-    user.create_user()
--- db_mysql/init_sql/1_init.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS myproject_nutrition_fact;
-CREATE DATABASE myproject_nutrition_fact;
