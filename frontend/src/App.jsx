6d86c282ecf913458f2a33fd9c12bf8ae05a9556
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-08-14 17:08:02 +0900

    App.jsxにfunc_Testを追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,41 @@
 // import React はJSXファイルをReact.createElement()に変換する
 // つまりReact.createElement()の記述をなくせる？
 import React from "react";
-import axios from "axios";
+// import axios from "axios";
 import './App.css';
 
 // Reactコンポ―ネントはHelloWorldのようなパスカルケースを好む
 
 // Appをappにしたらエラーになる
-function App() {
-	const [data, setData] = React.useState();
-	const url = "http://127.0.0.1:80";
+// function App() {
+// 	const [data, setData] = React.useState();
+// 	const url = "http://127.0.0.1:80";
 
-	const GetData = () => {
-		axios.get(url).then((res) => {
-			setData(res.data);
-		});
-	};
+// 	const GetData = () => {
+// 		axios.get(url).then((res) => {
+// 			setData(res.data);
+// 		});
+// 	};
+// 	return (
+// 		<div>
+// 			<div>ここに処理を書いていきます</div>
+// 			{data ? <div>{data.Hello}</div> : <button onClick={GetData}>データを取得</button>}
+// 		</div>
+// 	);
+// }
+
+function Test() {
+	const data = "React";
+
 	return (
-		<div>
-			<div>ここに処理を書いていきます</div>
-			{data ? <div>{data.Hello}</div> : <button onClick={GetData}>データを取得</button>}
-		</div>
+		<div className="App">
+			<p>
+				Hello, {data}!
+			</p>
+		</div>	
 	);
 }
 
 // Appコンポーネントを他のモジュールでも使えるようにする
-export default App;
+// export default App;
+export default Test;
