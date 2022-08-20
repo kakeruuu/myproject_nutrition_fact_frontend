a840e528a59016d2a13fff8f2c89574d33da4163
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-08-20 17:38:06 +0900

    MUIのサインインテンプレを参考にテキストボックスと送信ボタンを成型した。

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,67 @@
 import React from "react";
+// import Accordion from '@mui/material/Accordion';
+// import AccordionSummary from '@mui/material/AccordionSummary';
+// import AccordionDetails from '@mui/material/AccordionDetails';
 import Box from "@mui/material/Box"
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+// import Stack from '@mui/material/Stack';
 import TextField from "@mui/material/TextField";
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import { ExpandMore } from '@mui/icons-material';
+// import Typography from '@mui/material/Typography';
+// import { ExpandMore } from '@mui/icons-material';
 // import { ExpandLessIcon } from '@mui/icons-material';
 
 
 function App() {
   return (
     <div className="App">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        {/* TODO：テキストフィールドとアコーディオンの距離を開ける */}
-        {/* TODO：テキストフィールドとアコーディオンをBoxにまとめる。
-                  その方が細かい調整が効かせやすい？ */}
-        <TextField id="standard-basic" label="Food Name" variant="standard" />
+      <Container component="main" maxWidth="md">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          {/* TODO:Buttonの送信先について考える */}
+          {/* ReactでHttpレスポンスをどのように扱うのかを調べる */}
+          <Box component="form" noValidate sx={{ mt:1 }}>
+            <TextField 
+              margin="normal"
+              fullWidth
+              required
+              id="foodName"
+              label="Food Name"
+              variant="standard"
+            />
+            <Button 
+              type="submit"
+              fullWidth
+              variant="outlined"
+              sx={{ mt: 3, mb:2 }}
+            >
+              送信
+            </Button>
+          </Box>
 
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-            aria_controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography>詳しく</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              ここに内容を書く
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria_controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>詳しく</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                ここに内容を書く
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
 
-      </Box>
+        </Box>
+      </Container>
     </div>
   );
 }
