218472e9224d6698dea7c3cf4db2468379916fa5
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-08-23 18:43:10 +0900

    ホットリロードができるように変更した。

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-// import Accordion from '@mui/material/Accordion';
-// import AccordionSummary from '@mui/material/AccordionSummary';
-// import AccordionDetails from '@mui/material/AccordionDetails';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
 import Box from "@mui/material/Box"
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 // import Stack from '@mui/material/Stack';
 import TextField from "@mui/material/TextField";
-// import Typography from '@mui/material/Typography';
-// import { ExpandMore } from '@mui/icons-material';
+import Typography from '@mui/material/Typography';
+import { ExpandMore } from '@mui/icons-material';
 // import { ExpandLessIcon } from '@mui/icons-material';
 
 
-function App() {
+export function App() {
   return (
     <div className="App">
       <Container component="main" maxWidth="md">
@@ -66,4 +66,3 @@ function App() {
   );
 }
 
-export default App;
