7c8e919dcd0beea061e839b794671da9c2f98d1a
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-08-24 18:49:34 +0900

    ReactにてApiを呼び出す方法をテストするためのファイルを追加した。

---
+++ frontend/src/test.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+// import Accordion from '@mui/material/Accordion';
+// import AccordionSummary from '@mui/material/AccordionSummary';
+// import AccordionDetails from '@mui/material/AccordionDetails';
+// import Box from "@mui/material/Box"
+// import Button from '@mui/material/Button';
+// import Container from '@mui/material/Container';
+// import Stack from '@mui/material/Stack';
+// import TextField from "@mui/material/TextField";
+// import Typography from '@mui/material/Typography';
+// import { ExpandMore } from '@mui/icons-material';
+// import { ExpandLessIcon } from '@mui/icons-material';
+
+// コンポーネント名は常に大文字から開始する
+const ApiFetch = () => {
+
+}
+// TODO：Api呼び出し方法について調べているうちにuseStateやsetStateについて知る必要があるとわかり、
+// コンポーネントから改めて学習中。現在、「state を正しく使用する」まで学習。
+// 次回はそのあとから開始する
+
+
+export function test() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
