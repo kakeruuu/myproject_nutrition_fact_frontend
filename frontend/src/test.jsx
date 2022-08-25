5cba3de3560abd3e89f7a4c5446cd5b82d629554
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-08-25 20:44:57 +0900

    関数コンポーネントにフックを作成する練習まで。

--- frontend/src/test.jsx
+++ frontend/src/test.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 // import Accordion from '@mui/material/Accordion';
 // import AccordionSummary from '@mui/material/AccordionSummary';
 // import AccordionDetails from '@mui/material/AccordionDetails';
@@ -12,18 +12,27 @@ import React from "react";
 // import { ExpandLessIcon } from '@mui/icons-material';
 
 // コンポーネント名は常に大文字から開始する
-const ApiFetch = () => {
+// const ApiFetch = () => {
 
-}
+// }
 // TODO：Api呼び出し方法について調べているうちにuseStateやsetStateについて知る必要があるとわかり、
 // コンポーネントから改めて学習中。現在、「state を正しく使用する」まで学習。
 // 次回はそのあとから開始する
+// 関数コンポーネントのほうが推奨っぽい？
 
+export function Test() {
+  const [count, setCount] = useState(0)
 
-export function test() {
+  // マウント時・更新・アンマウント時
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  })
+
   return (
-    <div className="App">
-      
+    <div>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   );
 }
