3213af3a1e0d826902ee589468460d282f6c7a4b
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-08-27 17:58:14 +0900

    useStateを用いて、配列に文字列を追加してli表示するコードを追加

--- frontend/src/test.jsx
+++ frontend/src/test.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 // import Accordion from '@mui/material/Accordion';
 // import AccordionSummary from '@mui/material/AccordionSummary';
 // import AccordionDetails from '@mui/material/AccordionDetails';
@@ -12,30 +12,25 @@ import React, { useState,useEffect } from "react";
 // import { ExpandLessIcon } from '@mui/icons-material';
 
 // コンポーネント名は常に大文字から開始する
-// const ApiFetch = () => {
-
-// }
 // TODO：Api呼び出し方法について調べているうちにuseStateやsetStateについて知る必要があるとわかり、
 // コンポーネントから改めて学習中。現在、「state を正しく使用する」まで学習。
 // 次回はそのあとから開始する
 // 関数コンポーネントのほうが推奨っぽい？
 
-export function Test() {
-  const [count, setCount] = useState(0)
 
-  // マウント時・更新・アンマウント時
-  // TODO：リクエストを送る
-  useEffect(() => {
-    fetch("")
-      .then(response => response.json())
-      .then()
-  })
+export function Test() {
+  const [texts, setText] = useState(["test"])
 
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
+      <button onClick={() => setText([...texts, "test"])}>
+        テキスト追加
       </button>
+      <ul>
+        {texts.map((text, index) => {
+          return <li key={index}>{text}</li>
+        })}
+      </ul>
     </div>
   );
 }
