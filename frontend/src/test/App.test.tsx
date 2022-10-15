a76e90aceed626ae33ab28368a3674f438eda15f
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-10-15 11:34:44 +0900

    テストファイルの移動。

--- frontend/src/test/App.test.tsx
+++ frontend/src/test/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { App } from './components/App';
+import { App } from '../components/App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/メイン画面/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ frontend/src/test/postFoodList.test.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { render, screen } from '@testing-library/react';
+import { ReadFoodList } from '../components/api/postFoodList'
+
+// Testing Trophyの考えに基づくとこのテストは書いてはいけない？
+// →ユーザーから見えない部分だから
+test('postFoodListによってpostに正しい値が代入されるかどうか' ,() => {
+    const [post, setPost] = useState<any[]>([])
+    const query = {query: "にんじん,砂糖"}
+
+})
