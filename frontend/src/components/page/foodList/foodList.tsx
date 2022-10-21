800548bfdd2267560567d2dfc13e27d0ae5aa627
kakeruuu <kakeruuuuuu0114@gmail.com>
2022-10-21 11:28:17 +0900

    SwitchFoodListVisibleをコンポーネントとして呼び出す形に変更。

--- frontend/src/components/page/foodList/foodList.tsx
+++ frontend/src/components/page/foodList/foodList.tsx
@@ -1,10 +1,10 @@
-import React, { useState, createContext, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { FoodLists, FoodObject } from "../../../types/foodListType"
 import { PostsForm } from "./postsForm"
 import { SwitchFoodListVisible } from "./modules"
-// import { vitbleTest } from "./modules"
 import { UserSelectFoodNamesContext } from "./providers"
 
+// MEMO:userSelectFoodNamesの値が複数回呼び出されている
 export const FoodList = () => {
   const [posts, setPost] = useState<any>({})
   const updateSetPost = (obj: FoodLists | any): void => setPost(obj)
@@ -12,10 +12,10 @@ export const FoodList = () => {
   const keys: string[] = Object.keys(posts)
   const copyPosts: FoodLists = JSON.parse(JSON.stringify(posts))
   
-  const { userSelectFoodNames, setUserSelectFoodNames } = useContext(UserSelectFoodNamesContext)  
+  const { userSelectFoodNames, setUserSelectFoodNames } = useContext(UserSelectFoodNamesContext)
 
   // MEMO：userSelectFoodsの変数名は要検討
-  const [userSelectFoods, setUserSelectFoods] = useState<FoodLists>(copyPosts)
+  // const [userSelectFoods, setUserSelectFoods] = useState<FoodLists>(copyPosts)
   const [switchVisible, setSwitchVisible] = useState(true)
   
   // postからユーザーが選択したfoodIdのみ抽出する
@@ -27,7 +27,8 @@ export const FoodList = () => {
       tmpAry = posts[k].filter((p: {food_name: string}) => userSelectName.includes(p.food_name))
       if (tmpAry.length) {tmpObj[k] = tmpAry}
     })
-    setUserSelectFoods({...tmpObj})
+    // setUserSelectFoods({...tmpObj})
+    setPost({...tmpObj})
   };
 
   // postsとuserSelectFoodsを同一のものにした方が良い？
@@ -83,12 +84,22 @@ export const FoodList = () => {
     console.log(state)
   }
 
+  const switchProps = {
+    switchVisible: switchVisible,
+    posts: posts, 
+    userSelectFoodNames: userSelectFoodNames,
+    setUserSelectFoodNames:setUserSelectFoodNames
+  }
+
   return (
     <div>
       <PostsForm updateSetPost={updateSetPost}/>
       <div className="foodList">
-          {SwitchFoodListVisible(switchVisible, posts)}
-        {/* {switchFoodListVisible(switchVisible)} */}
+        {/* MEMO:postsとswitchVisibleの状態で表示される値が変わる変数。これ自体に出力を制御する機能があるわけではない。 */}
+        {/* つまり、以下の関数の視点で見るとpostsとswitchVisibleが勝手に変わったので出力するものが変わってしまった。くらいのニュアンス。 */}
+        {/* MEMO:foodlistを表示するコンポーネントとして定義したほうがいい？ */}
+        {/* {SwitchFoodListVisible(switchVisible, posts, userSelectFoodNames, setUserSelectFoodNames)} */}
+        <SwitchFoodListVisible switchProps={switchProps}/>
       </div>
       <div className="displaySelectFoodIds">
         {<ul>
@@ -98,6 +109,7 @@ export const FoodList = () => {
           })}
         </ul>}
       </div>
+      {/* 送信ボタンが押されるとpostの値がuserの選択した値のみになる */}
       <button type="submit" onClick={(e: any) => filterFoodIds(e, userSelectFoodNames)}>送信</button>
       <button type="submit" onClick={(e: any) => onClickObjCheck(e, posts)}>userSelectFoodsの値確認</button>
     </div>
--- frontend/src/components/page/foodList/modules.tsx
+++ frontend/src/components/page/foodList/modules.tsx
@@ -3,43 +3,60 @@ import { UserSelectFoodNamesContext } from "./providers"
 import { addStates } from "../../functional/states/addStates"
 
 // 状態変化を適切に考えないとおかしくなる
-export const SwitchFoodListVisible = (switchVisibleBool: boolean, ulObj: Record<string, string[]>) => {
-  const keys = Object.keys(ulObj)
+// propsにしようと思ったけど、呼び出し側でコンポーネント呼び出しになっていない
+// switchVisibleBool: boolean, postObj: Record<string, string[]>, state: any, setState: any
+// この状態だとpostsとuserSelectFoodNamesが空のためエラーを引き起こす可能性がある
+export const SwitchFoodListVisible = ({switchProps}: any) => {
+  // const keys = Object.keys(props.postObj)
+  const keys = Object.keys(switchProps.switchVisible)
   // 明示的にTypeを設定しないといけない。
-  const { userSelectFoodNames, setUserSelectFoodNames } = useContext(UserSelectFoodNamesContext)  
-  console.log(userSelectFoodNames)
-  
-  if (switchVisibleBool) {
-    return createUlWithTitle(keys, ulObj, userSelectFoodNames, setUserSelectFoodNames)
-  }
-  return createUlWithTitle(keys, ulObj)
-  // return keys.map((k, i) => {
-  //     return <ul key={i}>
-  //               <p>{k}</p>
-  //               {/* TODO:pのデータ型を考える */}
-  //               {/* {ulObj[k].map((p: any, idx: number) => {
-  //                 return <div key={idx}>{p[k]}</div>
-  //               })} */}
-  //             </ul>
-  //     })
+  console.log(switchProps.switchVisible)
+  console.log(switchProps.posts)
+  return <div>test</div>
+  // if (switchVisibleBool) {
+  //   return createUlWithTitle(keys, postObj, state, setState, switchVisibleBool)
+  // }
+  // return createUlWithTitle(keys, postObj, state, setState, switchVisibleBool)
 };
 
-// li要素作成のラッパー関数として定義できないだろうか？
-const createUlWithTitle = (keys: string[], ulObj: any, states: any = undefined, setStates: any = undefined) => {
+// MEMO:li要素作成関数から呼び出すラッパーとして定義できないだろうか？
+const createUlWithTitle = (keys: string[], postObj: any, states: any = undefined, setStates: any = undefined, switchVisibleBool: boolean) => {
+  function switchLi(k: string, postObj: any, states: any, setStates: any) {
+    if (switchVisibleBool) {
+      return createLi(k, postObj, states, setStates)
+    }
+    return createUserSelectLi(k, postObj)
+  }
+
   return keys.map((k: any, i: number) => {
-    return <ul key={i}>
-              <p>{k}</p>
-              {
-                createLi(k, ulObj, states, setStates)
-              }
-            </ul>
-            })
+    return (
+      <ul key={i}>
+        <p>{k}</p>
+        {
+          // ここでもスイッチしてるのおかしい
+          switchLi(k, postObj, states, setStates)
+        }
+      </ul>
+    )
+  })
 }
 
-const createLi = (key: any, ulObj: any, states: any, setStates: any) => {
-  return ulObj[key].map((p: any, idx: number) => {
+// ここでuseContextを呼び出すとエラーが出る↓
+// Warning: React has detected a change in the order of Hooks called by FoodList. This will lead to bugs and errors if not fixed. For more information, read the Rules of Hooks:
+const createLi = (key: any, postObj: any, states: any, setStates: any) => {
+  return postObj[key].map((p: any, idx: number) => {
     return <li key={idx} onClick={(e: any) => addStates(states, setStates, p.food_name)}>{p.food_name}</li>
   })
 }
 
 // userが選択したfood_nameの成分を表示する機能を表現した関数を作る
+// FIX:postsの状態を変化させた後に以下の関数で食材一覧が表示できないを直す
+// →表示されるタイミングの問題？
+// →渡されている値は正常だった。
+const createUserSelectLi = (key: any, postObj: any) => {
+  console.log("postObj= " + JSON.stringify(postObj))
+  console.log("key=" + String(key) + " type=" + typeof(key))
+  return postObj[key].map((p: any, idx: number) => {
+    return <li key={idx}>{p[key]}</li>
+  }) 
+}
